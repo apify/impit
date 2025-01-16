8cd94443339d0e494812a0219dc9bae4aad855cb
Jindřich Bär <jindrichbar@gmail.com>
2025-01-16 08:19:46 +0100

    chore: switch to `vitest` for testing `impit-node`

--- impit-node/test/basics.test.ts
+++ impit-node/test/basics.test.ts
@@ -1,20 +1,22 @@
-import test from 'ava'
+import { test } from 'vitest';
 
-import { Impit } from '../index.js'
+import { HttpMethod, Impit } from '../index.js'
 const impit = new Impit();
 
 test('impit works', async (t) => {
   const response = await impit.fetch('https://jindrich.bar');
 
   const text = await response.text();
-  text.includes('barjin') ? t.pass() : t.fail();
+  
+  t.expect(text).toContain('barjin');
 })
 
 test('json method works', async (t) => {
   const response = await impit.fetch('https://httpbin.org/json');
 
   const json = await response.json();
-  json.slideshow.author === 'Yours Truly' ? t.pass() : t.fail();
+
+  t.expect(json?.slideshow?.author).toBe('Yours Truly');
 })
 
 test('headers work', async (t) => {
@@ -22,42 +24,42 @@ test('headers work', async (t) => {
     'https://httpbin.org/headers', 
     { 
       headers: { 
-        'Retch-Test': 'foo',
+        'Impit-Test': 'foo',
         'Cookie': 'test=123; test2=456'
       } 
     }
   );
   const json = await response.json();
 
-  json.headers['Retch-Test'] ? t.pass() : t.fail();
+  t.expect(json.headers?.['Impit-Test']).toBe('foo');
 })
 
 test('string request body works', async (t) => {
   const response = await impit.fetch(
     'https://httpbin.org/post', 
     { 
-      method: 'POST',
-      body: '{"Retch-Test":"foořžš"}',
+      method: HttpMethod.Post,
+      body: '{"Impit-Test":"foořžš"}',
       headers: { 'Content-Type': 'application/json' }
     }
   );
   const json = await response.json();
 
-  json.data === '{"Retch-Test":"foořžš"}' ? t.pass() : t.fail();
+  t.expect(json.data).toEqual('{"Impit-Test":"foořžš"}');
 });
 
 test('binary request body works', async (t) => {
   const response = await impit.fetch(
     'https://httpbin.org/post', 
     { 
-      method: 'POST',
-      body: Uint8Array.from([0x52, 0x65, 0x74, 0x63, 0x68, 0x2d, 0x54, 0x65, 0x73, 0x74, 0x3a, 0x66, 0x6f, 0x6f, 0xc5, 0x99, 0xc5, 0xbe, 0xc5, 0xa1]),
+      method: HttpMethod.Post,
+      body: Uint8Array.from([0x49, 0x6d, 0x70, 0x69, 0x74, 0x2d, 0x54, 0x65, 0x73, 0x74, 0x3a, 0x66, 0x6f, 0x6f, 0xc5, 0x99, 0xc5, 0xbe, 0xc5, 0xa1]),
       headers: { 'Content-Type': 'application/json' }
     }
   );
   const json = await response.json();
 
-  t.deepEqual(json.data, 'Retch-Test:foořžš');
+  t.expect(json.data).toEqual('Impit-Test:foořžš');
 });
 
 test('redirects work by default', async (t) => {
@@ -65,7 +67,7 @@ test('redirects work by default', async (t) => {
     'https://httpbin.org/absolute-redirect/1', 
   );
 
-  t.deepEqual(response.status, 200);
+  t.expect(response.status).toBe(200);
 });
 
 test('disabling redirects works', async (t) => {
@@ -77,8 +79,8 @@ test('disabling redirects works', async (t) => {
     'https://httpbin.org/absolute-redirect/1', 
   );
 
-  t.deepEqual(response.status, 302);
-  t.deepEqual(response.headers['location'], 'http://httpbin.org/get');
+  t.expect(response.status).toBe(302);
+  t.expect(response.headers['location']).toBe('http://httpbin.org/get');
 });
 
 test('limiting redirects works', async (t) => {
@@ -91,5 +93,5 @@ test('limiting redirects works', async (t) => {
     'https://httpbin.org/absolute-redirect/2', 
   );
 
-  await t.throwsAsync(response, { message: /TooManyRedirects/ });
+  t.expect(response).rejects.toThrowError('TooManyRedirects');
 });
