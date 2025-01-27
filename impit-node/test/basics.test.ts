a2ebfd6330fafa7a1c2f88141d9abb6116feed09
Jindřich Bär <jindrichbar@gmail.com>
2025-01-27 17:32:02 +0100

    chore: add tests

--- impit-node/test/basics.test.ts
+++ impit-node/test/basics.test.ts
@@ -7,7 +7,7 @@ test('impit works', async (t) => {
   const response = await impit.fetch('https://jindrich.bar');
 
   const text = await response.text();
-  
+
   t.expect(text).toContain('barjin');
 })
 
@@ -21,12 +21,12 @@ test('json method works', async (t) => {
 
 test('headers work', async (t) => {
   const response = await impit.fetch(
-    'https://httpbin.org/headers', 
-    { 
-      headers: { 
+    'https://httpbin.org/headers',
+    {
+      headers: {
         'Impit-Test': 'foo',
         'Cookie': 'test=123; test2=456'
-      } 
+      }
     }
   );
   const json = await response.json();
@@ -36,8 +36,8 @@ test('headers work', async (t) => {
 
 test('string request body works', async (t) => {
   const response = await impit.fetch(
-    'https://httpbin.org/post', 
-    { 
+    'https://httpbin.org/post',
+    {
       method: HttpMethod.Post,
       body: '{"Impit-Test":"foořžš"}',
       headers: { 'Content-Type': 'application/json' }
@@ -50,8 +50,8 @@ test('string request body works', async (t) => {
 
 test('binary request body works', async (t) => {
   const response = await impit.fetch(
-    'https://httpbin.org/post', 
-    { 
+    'https://httpbin.org/post',
+    {
       method: HttpMethod.Post,
       body: Uint8Array.from([0x49, 0x6d, 0x70, 0x69, 0x74, 0x2d, 0x54, 0x65, 0x73, 0x74, 0x3a, 0x66, 0x6f, 0x6f, 0xc5, 0x99, 0xc5, 0xbe, 0xc5, 0xa1]),
       headers: { 'Content-Type': 'application/json' }
@@ -64,7 +64,7 @@ test('binary request body works', async (t) => {
 
 test('redirects work by default', async (t) => {
   const response = await impit.fetch(
-    'https://httpbin.org/absolute-redirect/1', 
+    'https://httpbin.org/absolute-redirect/1',
   );
 
   t.expect(response.status).toBe(200);
@@ -76,7 +76,7 @@ test('disabling redirects works', async (t) => {
   });
 
   const response = await impit.fetch(
-    'https://httpbin.org/absolute-redirect/1', 
+    'https://httpbin.org/absolute-redirect/1',
   );
 
   t.expect(response.status).toBe(302);
@@ -90,8 +90,27 @@ test('limiting redirects works', async (t) => {
   });
 
   const response = impit.fetch(
-    'https://httpbin.org/absolute-redirect/2', 
+    'https://httpbin.org/absolute-redirect/2',
   );
 
-  t.expect(response).rejects.toThrowError('TooManyRedirects');
+  await t.expect(response).rejects.toThrowError('TooManyRedirects');
+});
+
+test('streaming response body works', async (t) => {
+  const response = await impit.fetch(
+    'https://apify.github.io/impit/impit/index.html',
+  );
+
+  let found = false;
+
+  for await (const chunk of response.body) {
+    const text = new TextDecoder('utf-8', { fatal: false }).decode(chunk);
+
+    if (text.includes('impersonation')) {
+        found = true;
+        break;
+    }
+  }
+
+  t.expect(found).toBe(true);
 });
