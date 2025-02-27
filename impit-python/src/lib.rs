d8422f15e90f28ad1668a7400fc590961c274c95
Jindřich Bär <jindrichbar@gmail.com>
2025-02-27 14:22:07 +0100

    feat: add top-level functions (no Client usage)

--- impit-python/src/lib.rs
+++ impit-python/src/lib.rs
@@ -4,9 +4,60 @@ mod client;
 mod response;
 use client::Client;
 
+use std::collections::HashMap;
+
+macro_rules! http_no_client {
+    ($($name:ident),*) => {
+        $(
+            #[pyfunction]
+            fn $name(
+                url: String,
+                content: Option<Vec<u8>>,
+                data: Option<HashMap<String, String>>,
+                headers: Option<HashMap<String, String>>,
+                timeout: Option<f64>,
+            ) -> response::ImpitPyResponse {
+                let mut client = Client::new(None, None, None, None, None);
+
+                client.$name(url, content, data, headers, timeout)
+            }
+        )*
+    };
+}
+
+http_no_client!(
+    get,
+    post,
+    put,
+    head,
+    patch,
+    delete,
+    options,
+    trace
+);
+
 #[pymodule]
 fn impit(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Client>()?;
+
+    macro_rules! register_http_no_client {
+        ($($name:ident),*) => {
+            $(
+                m.add_function(wrap_pyfunction!($name, m)?)?;
+            )*
+        };
+    }
+
+    register_http_no_client!(
+        get,
+        post,
+        put,
+        head,
+        patch,
+        delete,
+        options,
+        trace
+    );
 
     Ok(())
 }
