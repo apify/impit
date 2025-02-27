1ab0256b3abf0d6803526e1fe286869fecf0cb61
Jindřich Bär <jindrichbar@gmail.com>
2025-02-27 15:22:07 +0100

    chore: simplify no client macros

--- impit-python/src/lib.rs
+++ impit-python/src/lib.rs
@@ -1,45 +1,39 @@
 use pyo3::prelude::*;
 
 mod client;
+mod async_client;
 mod response;
 use client::Client;
+use async_client::AsyncClient;
 
 use std::collections::HashMap;
-
-macro_rules! http_no_client {
-    ($($name:ident),*) => {
-        $(
-            #[pyfunction]
-            fn $name(
-                url: String,
-                content: Option<Vec<u8>>,
-                data: Option<HashMap<String, String>>,
-                headers: Option<HashMap<String, String>>,
-                timeout: Option<f64>,
-            ) -> response::ImpitPyResponse {
-                let mut client = Client::new(None, None, None, None, None);
-
-                client.$name(url, content, data, headers, timeout)
-            }
-        )*
-    };
-}
-
-http_no_client!(get, post, put, head, patch, delete, options, trace);
 
 #[pymodule]
 fn impit(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Client>()?;
 
-    macro_rules! register_http_no_client {
+    macro_rules! http_no_client {
         ($($name:ident),*) => {
             $(
+                #[pyfunction]
+                fn $name(
+                    url: String,
+                    content: Option<Vec<u8>>,
+                    data: Option<HashMap<String, String>>,
+                    headers: Option<HashMap<String, String>>,
+                    timeout: Option<f64>,
+                ) -> response::ImpitPyResponse {
+                    let mut client = Client::new(None, None, None, None, None);
+
+                    client.$name(url, content, data, headers, timeout)
+                }
+
                 m.add_function(wrap_pyfunction!($name, m)?)?;
             )*
         };
     }
 
-    register_http_no_client!(get, post, put, head, patch, delete, options, trace);
+    http_no_client!(get, post, put, head, patch, delete, options, trace);
 
     Ok(())
 }
