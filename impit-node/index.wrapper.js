552aa8ac658ec908f595dd124be944437a5c5b0f
Jindřich Bär <jindrichbar@gmail.com>
2025-05-07 15:29:02 +0200

    fix: render helpful error message on import errors (#171)
    Wraps the original `napi-rs` `ENOENT` error with a more descriptive error
    message. Understands `IMPIT_VERBOSE` envvar for printing the original
    error message.

--- impit-node/index.wrapper.js
+++ impit-node/index.wrapper.js
@@ -1,5 +1,21 @@
 const { castToTypedArray } = require('./request.js');
-const native = require('./index.js');
+let native = null;
+try {
+    native = require('./index.js');
+} catch (e) {
+    throw new Error(`
+impit couldn't load native bindings. 
+
+This can have several reasons:
+- The native bindings are not compiled for your platform (${process.platform}-${process.arch}).
+- You skipped installation of optional dependencies (using e.g. \`npm i --omit=optional\`).
+        While the main package (impit) still installs, your package manager will skip installing the prebuilt native bindings for your platform.
+        If you still want to skip installing other optional dependencies, please install the native bindings for your platform as a direct dependency of your project.
+- You are using a non-standard Node.js runtime (e.g. Deno, Bun, Cloudflare workers etc.) that might not support native modules.
+
+Run your script with IMPIT_VERBOSE=1 environment variable to get more information about the error.
+`, process.env['IMPIT_VERBOSE'] === '1' ? { cause: e } : undefined);
+}
 
 class ResponsePatches {
     static async text() {
