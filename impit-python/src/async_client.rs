216bf48e0f45467c850075a97bb20bf1facc2bdc
Jindřich Bär <jindrichbar@gmail.com>
2025-06-24 14:58:04 +0200

    feat(py): enable `cookies` param for `AsyncClient` constructor

--- impit-python/src/async_client.rs
+++ impit-python/src/async_client.rs
@@ -35,7 +35,7 @@ impl AsyncClient {
     }
 
     #[new]
-    #[pyo3(signature = (browser=None, http3=None, proxy=None, timeout=None, verify=None, default_encoding=None, follow_redirects=None, max_redirects=Some(20), cookie_jar=None))]
+    #[pyo3(signature = (browser=None, http3=None, proxy=None, timeout=None, verify=None, default_encoding=None, follow_redirects=None, max_redirects=Some(20), cookie_jar=None, cookies=None))]
     pub fn new(
         py: Python<'_>,
         browser: Option<String>,
@@ -47,6 +47,7 @@ impl AsyncClient {
         follow_redirects: Option<bool>,
         max_redirects: Option<u16>,
         cookie_jar: Option<crate::Bound<'_, crate::PyAny>>,
+        cookies: Option<crate::Bound<'_, crate::PyAny>>,
     ) -> Self {
         let builder = ImpitBuilder::default();
 
@@ -86,11 +87,17 @@ impl AsyncClient {
             _ => builder.with_redirect(impit::impit::RedirectBehavior::ManualRedirect),
         };
 
-        let builder = match cookie_jar {
-            Some(cookie_jar) => {
+        let builder = match (cookie_jar, cookies) {
+            (Some(_), Some(_)) => {
+                panic!("Both cookie_jar and cookies cannot be provided at the same time")
+            },
+            (Some(cookie_jar), None) => {
                 builder.with_cookie_store(PythonCookieJar::new(py, cookie_jar.into()))
-            }
-            None => builder,
+            },
+            (None, Some(cookies)) => {
+                builder.with_cookie_store(PythonCookieJar::from_httpx_cookies(py, cookies.into()))
+            },
+            (None, None) => builder,
         };
 
         Self {
