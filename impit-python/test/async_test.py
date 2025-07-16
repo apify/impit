8bb982ec779ac8dd833c3bc29a7a81a280b92c04
Max Bohomolov <34358312+Mantisus@users.noreply.github.com>
2025-07-16 12:03:53 +0300

    fix(py): fix cookies with attributes 'SameSite' and `domain` (#213)

--- impit-python/test/async_test.py
+++ impit-python/test/async_test.py
@@ -78,6 +78,61 @@ class TestBasicRequests:
         for cookie in cookies_jar:
             assert cookie.has_nonstandard_attr('HttpOnly') is not None
 
+    async def test_complex_cookies(self, browser: Browser) -> None:
+        cookies_jar = CookieJar()
+
+        impit = AsyncClient(browser=browser, cookie_jar=cookies_jar, follow_redirects=True)
+
+        url = get_httpbin_url(
+            '/response-headers',
+            query={
+                'set-cookie': [
+                    'basic=1; Path=/; HttpOnly; SameSite=Lax',
+                    'withpath=2; Path=/html; SameSite=None',
+                    'strict=3; Path=/; SameSite=Strict',
+                    'secure=4; Path=/; HttpOnly; Secure; SameSite=Strict',
+                    'short=5; Path=/;',
+                    'domain=6; Path=/; Domain=.127.0.0.1;',
+                ]
+            },
+        )
+
+        await impit.get(url)
+
+        assert len(cookies_jar) == 6
+        for cookie in cookies_jar:
+            if cookie.name == 'basic':
+                assert cookie.value == '1'
+                assert cookie.secure is False
+                assert cookie.has_nonstandard_attr('HttpOnly') is True
+                assert cookie.get_nonstandard_attr('SameSite') == 'Lax'
+            elif cookie.name == 'withpath':
+                assert cookie.value == '2'
+                assert cookie.secure is False
+                assert cookie.get_nonstandard_attr('SameSite') == 'None'
+                assert cookie.has_nonstandard_attr('HttpOnly') is False
+                assert cookie.path == '/html'
+            elif cookie.name == 'strict':
+                assert cookie.value == '3'
+                assert cookie.secure is False
+                assert cookie.has_nonstandard_attr('HttpOnly') is False
+                assert cookie.get_nonstandard_attr('SameSite') == 'Strict'
+            elif cookie.name == 'secure':
+                assert cookie.value == '4'
+                assert cookie.secure is True
+                assert cookie.has_nonstandard_attr('HttpOnly') is True
+                assert cookie.get_nonstandard_attr('SameSite') == 'Strict'
+            elif cookie.name == 'short':
+                assert cookie.value == '5'
+                assert cookie.secure is False
+                assert cookie.has_nonstandard_attr('SameSite') is False
+            elif cookie.name == 'domain':
+                assert cookie.value == '6'
+                assert cookie.secure is False
+                # Crate cookies, ignores the starting dot in the domain
+                # but it's ok - https://www.rfc-editor.org/rfc/rfc6265#section-4.1.2.3
+                assert cookie.domain == '127.0.0.1'
+
     @pytest.mark.asyncio
     async def test_cookie_jar_works(self, browser: Browser) -> None:
         cookies = Cookies({'preset-cookie': '123'})
