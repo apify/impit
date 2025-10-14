055ae12ae0bb1481155ea30324515062b7648287
Jindřich Bär <jindrichbar@gmail.com>
2025-10-14 11:03:41 +0200

    chore: fix mypy error

--- impit-python/python/impit/__init__.py
+++ impit-python/python/impit/__init__.py
@@ -1,7 +1,7 @@
 from importlib import metadata
 from typing import Literal
 
-from .cookies import Cookies
+from .cookies import Cookies as Cookies
 from .impit import (
     AsyncClient,
     Client,
