b75b566e54fbccffe8f0bb6fb4c4e0a44449e1dd
Jindřich Bär <jindrichbar@gmail.com>
2025-11-13 13:12:54 +0100

    fix: treat unexpected EOF error as `RemoteProtocolError` (#314)
    Related to https://github.com/apify/apify-sdk-python/issues/672

--- impit/src/errors.rs
+++ impit/src/errors.rs
@@ -113,6 +113,10 @@ impl ImpitError {
             return ImpitError::TooManyRedirects(context.max_redirects);
         }
 
+        if error.is_body() && (format!("{:?}", error).to_lowercase()).contains("unexpectedeof") {
+            return ImpitError::RemoteProtocolError;
+        }
+
         if error.is_request() {
             if let Some(source_error) = error
                 .source()
